67359bf02171a400d15df0498a34428e0232d550
Rodolfo Herman <ciencia.rodolfo@gmail.com>
2021-08-16 19:52:18 -0300

    Mudança dos ícones do Radio

--- src/assets/icons/CheckedRadioIcon.js
+++ src/assets/icons/CheckedRadioIcon.js
@@ -5,8 +5,9 @@ const CheckedRadioIcon = ({
     cursor,
     onClick,
 }) => (
-    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill={color} onClick={onClick} cursor={cursor} className="bi bi-disc-fill" viewBox="0 0 16 16">
-        <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-6 0a2 2 0 1 0-4 0 2 2 0 0 0 4 0zM4 8a4 4 0 0 1 4-4 .5.5 0 0 0 0-1 5 5 0 0 0-5 5 .5.5 0 0 0 1 0zm9 0a.5.5 0 1 0-1 0 4 4 0 0 1-4 4 .5.5 0 0 0 0 1 5 5 0 0 0 5-5z"/>
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill={color} onClick={onClick} cursor={cursor} className="bi bi-check-circle" viewBox="0 0 16 16">
+        <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+        <path d="M10.97 4.97a.235.235 0 0 0-.02.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-1.071-1.05z"/>
     </svg>
 )
 
